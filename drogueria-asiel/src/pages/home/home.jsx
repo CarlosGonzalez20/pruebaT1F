8667278a67619ef238751a0d50cd0d2134a31f34
CarlosGonzalez20 <ll3224645@gmail.com>
2025-03-21 17:40:03 -0600

    cambiando imagen de home a background

--- drogueria-asiel/src/pages/home/home.jsx
+++ drogueria-asiel/src/pages/home/home.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./home.css";
 import Navbar from "../../components/navbar/navbar";
-import medicamentos from "../../assets/Images/medicamentos.webp"; // Importa la imagen
 
 const Home = () => {
     return (
         <div className="home">
             <Navbar />
-            <img src={medicamentos} alt="medicamentos" className="medicamentos" />
+            <div className="medicamentos" />
         </div>
     );
 };
