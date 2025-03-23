92bb000fe88a3ea551fab0f438794ae83f0c8449
CarlosGonzalez20 <ll3224645@gmail.com>
2025-03-22 18:07:11 -0600

    agregando speed insghts para monitoreo temporal

--- drogueria-asiel/src/App.jsx
+++ drogueria-asiel/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/home/home'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <SpeedInsights />
       <Home />
     </>
   )
