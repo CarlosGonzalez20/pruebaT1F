5f010481310a3f65b6adf45f8cf9a61d310546c8
CarlosGonzalez20 <ll3224645@gmail.com>
2025-04-01 19:45:47 -0600

    agregando loading para cuando sea necesario

--- drogueria-asiel/src/App.jsx
+++ drogueria-asiel/src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home/home'
-import Error404 from './pages/notFound/notFound'
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import './App.css';
+
+// Componentes cargados dinámicamente
+const Home = lazy(() => import('./pages/home/home'));
+const Error404 = lazy(() => import('./pages/notFound/notFound'));
+const Loader = lazy(() => import('./components/loader/loader'));
 
 function App() {
   return (
-    <>
+    <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
-    </>
-  )
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
