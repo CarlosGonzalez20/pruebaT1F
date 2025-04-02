e71dec9791c3d52917a26a1840c81acf42fb475e
CarlosGonzalez20 <ll3224645@gmail.com>
2025-04-02 11:44:22 -0600

    agregando dsiponibilidad de cuenta para comentar

--- drogueria-asiel/src/components/navbar/navbar.jsx
+++ drogueria-asiel/src/components/navbar/navbar.jsx
@@ -9,7 +9,26 @@ const NAV_LINKS = [
     { path: "/nosotros", text: "SOBRE NOSOTROS" },
     { path: "/productos", text: "PRODUCTOS" },
     { path: "/contacto", text: "CONTÁCTENOS" },
-    { path: "/blog", text: "BLOG" }
+    { path: "/blog", text: "BLOG" },
+    { 
+        path: "/cuenta", 
+        icon: (
+            <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                strokeWidth={1.5} 
+                stroke="currentColor" 
+                className="account-icon"
+            >
+                <path 
+                    strokeLinecap="round" 
+                    strokeLinejoin="round" 
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" 
+                />
+            </svg>
+        ) 
+    },
 ];
 
 const Navbar = memo(() => {
@@ -48,10 +67,11 @@ const Navbar = memo(() => {
                     <li key={link.path}>
                         <Link 
                             to={link.path} 
-                            className="menu-link"
+                            className={`menu-link ${link.icon ? 'with-icon' : ''}`}
                             onClick={handleLinkClick}
+                            aria-label={link.icon ? 'Cuenta' : link.text}
                         >
-                            {link.text}
+                            {link.icon || link.text}
                         </Link>
                     </li>
                 ))}
