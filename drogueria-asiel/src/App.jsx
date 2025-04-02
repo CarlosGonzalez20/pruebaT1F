caa3a8856591100630680fdb2d57377861a0bea6
CarlosGonzalez20 <ll3224645@gmail.com>
2025-04-02 12:59:45 -0600

    corrigiendo errores en el app.jsx con suspense y loader, regresando contenedor padre a App y no a home

--- drogueria-asiel/src/App.jsx
+++ drogueria-asiel/src/App.jsx
@@ -1,22 +1,21 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-
-// Loader estático (mejor que lazy para el fallback)
 import Loader from './components/loader/loader';
 
-// Rutas dinámicas con prefetch implícito
-const Home = lazy(() => import(/* webpackPrefetch: true */ './pages/home/home'));
+const Home = lazy(() => import('./pages/home/home'));
 const Error404 = lazy(() => import('./pages/notFound/notFound'));
 
 function App() {
   return (
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
-    </Suspense>
+    <div className="app-container">
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </Suspense>
+    </div>
   );
 }
 
